bedd32c2e606fd4f713dbef39cb0fa1833cd58e1
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-02-18 21:10:34 +0700

    fixed a mistake

--- pages/api/employee/[id].js
+++ pages/api/employee/[id].js
@@ -3,7 +3,7 @@ import { promises as fs } from "fs";
 export default async function handler(req, res) {
   const jsonDirectory = path.join(process.cwd(), "data");
   const fileContents = await fs.readFile(jsonDirectory + "/employees.json", "utf8");
-  const products = JSON.parse(fileContents);
+  const employees = JSON.parse(fileContents);
 
   const p = employees.find((employee) => employee.id == req.query.id);
   console.debug(req.query.id, p);
